075c4da9416feb73de828204a5889f2e66a5bd3d
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-10 16:19:16 +0800

    抽取BaseService实现

--- pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou/pinyougou-sellergoods/pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -5,13 +5,14 @@ import com.github.pagehelper.PageHelper;
 import com.pinyougou.mapper.BrandMapper;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
+import com.pinyougou.service.impl.BaseServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 //将服务暴露注册到dubbo注册中心
 @Service(interfaceClass = BrandService.class)
-public class BrandServiceImpl implements BrandService {
+public class BrandServiceImpl extends BaseServiceImpl<TbBrand> implements BrandService {
 
     @Autowired
     private BrandMapper brandMapper;
