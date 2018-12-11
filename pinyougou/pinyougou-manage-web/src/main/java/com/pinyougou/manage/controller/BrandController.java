20bba1b2b6ea6a6530cafe8a5fb63204cf8b12aa
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-11 15:33:51 +0800

    品牌新增

--- pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
+++ pinyougou/pinyougou-manage-web/src/main/java/com/pinyougou/manage/controller/BrandController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 import com.pinyougou.pojo.TbBrand;
 import com.pinyougou.sellergoods.service.BrandService;
 import com.pinyougou.vo.PageResult;
+import com.pinyougou.vo.Result;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,25 @@ public class BrandController {
     //注入代理对象
     @Reference
     private BrandService brandService;
+
+    /**
+     * 保存品牌
+     * @param brand 品牌
+     * @return 操作结果
+     */
+    @PostMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+
+        try {
+            brandService.add(brand);
+
+            return Result.ok("新增品牌成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return Result.fail("新增品牌失败");
+    }
 
     /**
      * 分页查询品牌
