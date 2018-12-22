ff425a052603fd846690999240715f86ba5522b4
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-22 18:52:39 +0800

    搜索关键字高亮显示

--- pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou/pinyougou-search/pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -6,10 +6,15 @@ import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
 import org.springframework.data.solr.core.query.Criteria;
+import org.springframework.data.solr.core.query.HighlightOptions;
+import org.springframework.data.solr.core.query.SimpleHighlightQuery;
 import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -23,16 +28,45 @@ public class ItemSearchServiceImpl implements ItemSearchService {
         Map<String, Object> resultMap = new HashMap<String, Object>();
 
         //创建查询对象
-        SimpleQuery query = new SimpleQuery();
+        //SimpleQuery query = new SimpleQuery();
+        //创建高亮的查询对象
+        SimpleHighlightQuery query = new SimpleHighlightQuery();
 
         //设置查询条件 is 会对搜索关键字进行分词
         Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
 
-        //查询
-        ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+        //设置高亮的域名和起始、结束标签
+        HighlightOptions highlightOptions = new HighlightOptions();
+        //高亮域名
+        highlightOptions.addField("item_title");
+        //设置高亮的起始标签
+        highlightOptions.setSimplePrefix("<font style='color:red'>");
+        //设置高亮的结束标签
+        highlightOptions.setSimplePostfix("</font>");
 
-        resultMap.put("rows", scoredPage.getContent());
+        query.setHighlightOptions(highlightOptions);
+
+        //查询
+        //ScoredPage<TbItem> scoredPage = solrTemplate.queryForPage(query, TbItem.class);
+        HighlightPage<TbItem> highlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
+
+        //处理高亮的返回结果
+        List<HighlightEntry<TbItem>> highlighted = highlightPage.getHighlighted();
+        if (highlighted != null && highlighted.size() > 0) {
+            for (HighlightEntry<TbItem> entry : highlighted) {
+                List<HighlightEntry.Highlight> highlights = entry.getHighlights();
+                if (highlights != null && highlights.size() > 0
+                        && highlights.get(0).getSnipplets() != null && highlights.get(0).getSnipplets().size() > 0) {
+                    //第一个get(0)为获取第一个域，第二个get(0)获取该域第一个高亮的字符串
+                    String title = highlights.get(0).getSnipplets().get(0).toString();
+                    entry.getEntity().setTitle(title);
+                }
+            }
+        }
+
+
+        resultMap.put("rows", highlightPage.getContent());
         return resultMap;
     }
 }
