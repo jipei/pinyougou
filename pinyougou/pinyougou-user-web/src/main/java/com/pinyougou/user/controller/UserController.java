f0da4f5a8730561ab3ba12be7b2dd4474ce954d5
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-29 12:05:32 +0800

    用户注册实现

--- pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
+++ pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
@@ -5,8 +5,10 @@ import com.pinyougou.pojo.TbUser;
 import com.pinyougou.user.service.UserService;
 import com.pinyougou.vo.PageResult;
 import com.pinyougou.vo.Result;
+import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RequestMapping("/user")
@@ -27,15 +29,24 @@ public class UserController {
         return userService.findPage(page, rows);
     }
 
+    /**
+     * 验证验证码后保存用户信息到数据库表中
+     * @param user 用户信息
+     * @param smsCode 用户输入验证码
+     * @return 操作结果
+     */
     @PostMapping("/add")
-    public Result add(@RequestBody TbUser user) {
+    public Result add(@RequestBody TbUser user, String smsCode) {
         try {
+            user.setCreated(new Date());
+            user.setUpdated(user.getCreated());
+            user.setPassword(DigestUtils.md5Hex(user.getPassword()));
             userService.add(user);
-            return Result.ok("增加成功");
+            return Result.ok("注册成功");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return Result.fail("增加失败");
+        return Result.fail("注册失败");
     }
 
     @GetMapping("/findOne")
