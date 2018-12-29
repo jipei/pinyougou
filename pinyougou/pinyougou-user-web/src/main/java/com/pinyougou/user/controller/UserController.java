4d0479cba324a187798a587c9e7907c3fd2a78e5
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-29 12:41:01 +0800

    发送短信验证码实现

--- pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
+++ pinyougou/pinyougou-user-web/src/main/java/com/pinyougou/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.pinyougou.user.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou.common.util.PhoneFormatCheckUtils;
 import com.pinyougou.pojo.TbUser;
 import com.pinyougou.user.service.UserService;
 import com.pinyougou.vo.PageResult;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
 @RequestMapping("/user")
 @RestController
@@ -17,6 +19,28 @@ public class UserController {
 
     @Reference
     private UserService userService;
+
+    /**
+     * 发送短信验证码
+     * @param phone 手机号
+     * @return 操作结果
+     */
+    @GetMapping("/sendSmsCode")
+    public Result sendSmsCode(String phone){
+        Result result = Result.fail("发送短信验证码失败");
+        try {
+            if (PhoneFormatCheckUtils.isPhoneLegal(phone)) {
+                //发送短信验证码
+                userService.sendSmsCode(phone);
+                result = Result.ok("发送验证码成功！");
+            } else {
+                result = Result.fail("手机号码不正确，发送验证码失败！");
+            }
+        } catch (PatternSyntaxException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
 
     @RequestMapping("/findAll")
     public List<TbUser> findAll() {
--- pinyougou/pinyougou-user/pinyougou-user-interface/src/main/java/com/pinyougou/user/service/UserService.java
+++ pinyougou/pinyougou-user/pinyougou-user-interface/src/main/java/com/pinyougou/user/service/UserService.java
@@ -7,4 +7,10 @@ import com.pinyougou.vo.PageResult;
 public interface UserService extends BaseService<TbUser> {
 
     PageResult search(Integer page, Integer rows, TbUser user);
+
+    /**
+     * 发送短信验证码
+     * @param phone 手机号
+     */
+    void sendSmsCode(String phone);
 }
