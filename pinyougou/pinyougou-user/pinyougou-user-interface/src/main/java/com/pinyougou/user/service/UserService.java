d3c2e87781f34eeac6599137e9e501a4384fa9b7
liaojianbin <meinitcasthavefun@foxmail.com>
2018-12-29 11:39:12 +0800

    搭建用户中心系统

---
+++ pinyougou/pinyougou-user/pinyougou-user-interface/src/main/java/com/pinyougou/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.pinyougou.user.service;
+
+import com.pinyougou.pojo.TbUser;
+import com.pinyougou.service.BaseService;
+import com.pinyougou.vo.PageResult;
+
+public interface UserService extends BaseService<TbUser> {
+
+    PageResult search(Integer page, Integer rows, TbUser user);
+}
---
+++ pinyougou/pinyougou-user/pinyougou-user-service/src/main/java/com/pinyougou/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.pinyougou.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.pinyougou.mapper.UserMapper;
+import com.pinyougou.pojo.TbUser;
+import com.pinyougou.user.service.UserService;
+import com.pinyougou.service.impl.BaseServiceImpl;
+import com.pinyougou.vo.PageResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
+
+@Service(interfaceClass = UserService.class)
+public class UserServiceImpl extends BaseServiceImpl<TbUser> implements UserService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public PageResult search(Integer page, Integer rows, TbUser user) {
+        PageHelper.startPage(page, rows);
+
+        Example example = new Example(TbUser.class);
+        Example.Criteria criteria = example.createCriteria();
+        /*if(!StringUtils.isEmpty(user.get***())){
+            criteria.andLike("***", "%" + user.get***() + "%");
+        }*/
+
+        List<TbUser> list = userMapper.selectByExample(example);
+        PageInfo<TbUser> pageInfo = new PageInfo<>(list);
+
+        return new PageResult(pageInfo.getTotal(), pageInfo.getList());
+    }
+}
