0347ab08e64beb017c55934131982fc09b215305
liaojianbin <meinitcasthavefun@foxmail.com>
2019-01-07 10:20:53 +0800

    秒杀首页商品数据缓存加载实现

--- pinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillGoodsController.java
+++ pinyougou/pinyougou-seckill-web/src/main/java/com/pinyougou/seckill/controller/SeckillGoodsController.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RestController
 public class SeckillGoodsController {
 
-    @Reference
+    @Reference(timeout = 100000)
     private SeckillGoodsService seckillGoodsService;
 
     /**
--- pinyougou/pinyougou-seckill/pinyougou-seckill-service/src/main/java/com/pinyougou/seckill/service/impl/SeckillGoodsServiceImpl.java
+++ pinyougou/pinyougou-seckill/pinyougou-seckill-service/src/main/java/com/pinyougou/seckill/service/impl/SeckillGoodsServiceImpl.java
@@ -9,6 +9,7 @@ import com.pinyougou.seckill.service.SeckillGoodsService;
 import com.pinyougou.service.impl.BaseServiceImpl;
 import com.pinyougou.vo.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.StringUtils;
 import tk.mybatis.mapper.entity.Example;
 
@@ -18,8 +19,14 @@ import java.util.List;
 @Service(interfaceClass = SeckillGoodsService.class)
 public class SeckillGoodsServiceImpl extends BaseServiceImpl<TbSeckillGoods> implements SeckillGoodsService {
 
+    //秒杀商品列表在redis中key的名称
+    private static final String SECKILL_GOODS = "SECKILL_GOODS";
+
     @Autowired
     private SeckillGoodsMapper seckillGoodsMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public PageResult search(Integer page, Integer rows, TbSeckillGoods seckillGoods) {
@@ -40,6 +47,18 @@ public class SeckillGoodsServiceImpl extends BaseServiceImpl<TbSeckillGoods> imp
     @Override
     public List<TbSeckillGoods> findList() {
         List<TbSeckillGoods> seckillGoodsList = null;
+        try {
+            // 1.1、从redis中获取到秒杀商品列表，如果存在则直接返回；
+            seckillGoodsList = redisTemplate.boundHashOps(SECKILL_GOODS).values();
+            if (seckillGoodsList != null && seckillGoodsList.size() > 0) {
+                return seckillGoodsList;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //    1.2、到数据库中查询秒杀商品列表；
+
         //剩余库存大于0，已审核，开始时间小于等于当前时间，结束时间大于当前时间；并且按照开始时间升序排序
 
         Example example = new Example(TbSeckillGoods.class);
@@ -60,6 +79,17 @@ public class SeckillGoodsServiceImpl extends BaseServiceImpl<TbSeckillGoods> imp
 
         //查询
         seckillGoodsList = seckillGoodsMapper.selectByExample(example);
+
+        try {
+            //1.3、遍历秒杀商品列表逐个逐个地将秒杀商品存入到redis中（以hash结构存储）
+            if(seckillGoodsList != null && seckillGoodsList.size() > 0){
+                for (TbSeckillGoods seckillGoods : seckillGoodsList) {
+                    redisTemplate.boundHashOps(SECKILL_GOODS).put(seckillGoods.getId(), seckillGoods);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return seckillGoodsList;
     }
